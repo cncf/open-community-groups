01140a23739f4fd9c035778f1bdc60e2023dee28
Sergio Castaño Arteaga <tegioz@icloud.com>
2024-08-26 11:44:37 +0200

    Add explore groups and events endpoints
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- ocg-hypermedia-api/src/handlers/community.rs
+++ ocg-hypermedia-api/src/handlers/community.rs
@@ -1,8 +1,11 @@
 //! This module defines the HTTP handlers for the community site.
 
+use std::collections::HashMap;
+
 use super::extractor::Community;
 use askama::Template;
 use askama_axum::IntoResponse;
+use axum::extract::Query;
 use tracing::debug;
 
 /// Index document template.
@@ -21,12 +24,39 @@ pub(crate) async fn index(Community(community): Community) -> impl IntoResponse 
 /// Explore page template.
 #[derive(Debug, Clone, Template)]
 #[template(path = "community/explore.html")]
-pub(crate) struct Explore {}
+pub(crate) struct Explore {
+    params: HashMap<String, String>,
+}
 
 /// Handler that returns the explore page.
 #[allow(clippy::unused_async)]
-pub(crate) async fn explore(Community(community): Community) -> impl IntoResponse {
-    debug!("community: {}", community);
+pub(crate) async fn explore(
+    Community(community): Community,
+    Query(params): Query<HashMap<String, String>>,
+) -> impl IntoResponse {
+    debug!("community: {}, params: {:?}", community, params);
 
-    Explore {}
+    Explore { params }
+}
+
+/// Explore events section template.
+#[derive(Debug, Clone, Template)]
+#[template(path = "community/events.html")]
+pub(crate) struct Events {}
+
+/// Handler that returns the explore events section.
+#[allow(clippy::unused_async)]
+pub(crate) async fn events(Community(_community): Community) -> impl IntoResponse {
+    Events {}
+}
+
+/// Explore groups section template.
+#[derive(Debug, Clone, Template)]
+#[template(path = "community/groups.html")]
+pub(crate) struct Groups {}
+
+/// Handler that returns the explore groups section.
+#[allow(clippy::unused_async)]
+pub(crate) async fn groups(Community(_community): Community) -> impl IntoResponse {
+    Groups {}
 }
