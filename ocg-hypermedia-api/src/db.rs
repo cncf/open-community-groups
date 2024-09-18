67931a3c904a3ad1d71084e81ae4b2e7c0d89277
Sergio Castaño Arteaga <tegioz@icloud.com>
2024-09-18 13:49:46 +0200

    Add data for community index template
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- ocg-hypermedia-api/src/db.rs
+++ ocg-hypermedia-api/src/db.rs
@@ -6,12 +6,17 @@ use deadpool_postgres::Pool;
 use std::sync::Arc;
 use uuid::Uuid;
 
+use crate::handlers::community;
+
 /// Abstraction layer over the database. Trait that defines some operations a
 /// DB implementation must support.
 #[async_trait]
 pub(crate) trait DB {
     /// Get the community id from the host provided.
     async fn get_community_id(&self, host: &str) -> Result<Option<Uuid>>;
+
+    /// Get data for the community index template.
+    async fn get_community_index_data(&self, community_id: Uuid) -> Result<community::Index>;
 }
 
 /// Type alias to represent a DB trait object.
@@ -43,5 +48,69 @@ impl DB for PgDB {
             .map(|row| row.get("community_id"));
 
         Ok(community_id)
+    }
+
+    /// [DB::get_community_index_data]
+    async fn get_community_index_data(&self, community_id: Uuid) -> Result<community::Index> {
+        // Get community index data from database
+        let db = self.pool.get().await?;
+        let row = db
+            .query_one(
+                "
+                select
+                    banners_urls,
+                    copyright_notice,
+                    description,
+                    display_name,
+                    extra_links,
+                    facebook_url,
+                    flickr_url,
+                    footer_logo_url,
+                    github_url,
+                    header_logo_url,
+                    homepage_url,
+                    instagram_url,
+                    linkedin_url,
+                    photos_urls,
+                    slack_url,
+                    twitter_url,
+                    wechat_url,
+                    youtube_url
+                from community
+                where community_id = $1
+                ",
+                &[&community_id],
+            )
+            .await?;
+
+        // Prepare community index data
+        let mut index = community::Index {
+            banners_urls: row.get("banners_urls"),
+            copyright_notice: row.get("copyright_notice"),
+            description: row.get("description"),
+            display_name: row.get("display_name"),
+            extra_links: None,
+            facebook_url: row.get("facebook_url"),
+            flickr_url: row.get("flickr_url"),
+            footer_logo_url: row.get("footer_logo_url"),
+            github_url: row.get("github_url"),
+            header_logo_url: row.get("header_logo_url"),
+            homepage_url: row.get("homepage_url"),
+            instagram_url: row.get("instagram_url"),
+            linkedin_url: row.get("linkedin_url"),
+            photos_urls: row.get("photos_urls"),
+            slack_url: row.get("slack_url"),
+            twitter_url: row.get("twitter_url"),
+            wechat_url: row.get("wechat_url"),
+            youtube_url: row.get("youtube_url"),
+        };
+        let extra_links: Option<serde_json::Value> = row.get("extra_links");
+        if let Some(extra_links) = extra_links {
+            if let Ok(extra_links) = serde_json::from_value(extra_links) {
+                index.extra_links = Some(extra_links);
+            }
+        }
+
+        Ok(index)
     }
 }
--- ocg-hypermedia-api/src/handlers/community.rs
+++ ocg-hypermedia-api/src/handlers/community.rs
@@ -1,31 +1,60 @@
 //! This module defines the HTTP handlers for the community site.
 
 use super::extractor::CommunityId;
+use crate::db::DynDB;
+use anyhow::Error;
 use askama::Template;
 use askama_axum::IntoResponse;
-use axum::extract::Query;
-use std::collections::HashMap;
-use tracing::debug;
+use axum::{
+    extract::{Query, State},
+    http::StatusCode,
+};
+use serde::Serialize;
+use std::{collections::HashMap, fmt::Display};
+use tracing::{debug, error};
 
 /// Index document template.
-#[derive(Debug, Clone, Template)]
+#[derive(Debug, Clone, Template, Serialize)]
 #[template(path = "community/index.html")]
-pub(crate) struct Index {}
+#[allow(dead_code)]
+pub(crate) struct Index {
+    pub banners_urls: Option<Vec<String>>,
+    pub copyright_notice: Option<String>,
+    pub description: Option<String>,
+    pub display_name: String,
+    pub extra_links: Option<HashMap<String, String>>,
+    pub facebook_url: Option<String>,
+    pub flickr_url: Option<String>,
+    pub footer_logo_url: Option<String>,
+    pub github_url: Option<String>,
+    pub header_logo_url: Option<String>,
+    pub homepage_url: Option<String>,
+    pub instagram_url: Option<String>,
+    pub linkedin_url: Option<String>,
+    pub photos_urls: Option<Vec<String>>,
+    pub slack_url: Option<String>,
+    pub twitter_url: Option<String>,
+    pub wechat_url: Option<String>,
+    pub youtube_url: Option<String>,
+}
 
 /// Handler that returns the index document.
 #[allow(clippy::unused_async)]
-pub(crate) async fn index(CommunityId(community_id): CommunityId) -> impl IntoResponse {
-    debug!("community_id: {}", community_id);
-
-    Index {}
+pub(crate) async fn index(
+    State(db): State<DynDB>,
+    CommunityId(community_id): CommunityId,
+) -> impl IntoResponse {
+    db.get_community_index_data(community_id)
+        .await
+        .map_err(internal_error)
 }
 
 /// Explore page template.
 #[derive(Debug, Clone, Template)]
 #[template(path = "community/explore.html")]
+#[allow(dead_code)]
 pub(crate) struct Explore {
-    #[allow(dead_code)]
-    params: HashMap<String, String>,
+    pub params: HashMap<String, String>,
 }
 
 /// Handler that returns the explore page.
@@ -59,4 +88,14 @@ pub(crate) struct Groups {}
 #[allow(clippy::unused_async)]
 pub(crate) async fn groups(CommunityId(_community): CommunityId) -> impl IntoResponse {
     Groups {}
+}
+
+/// Helper for mapping any error into a `500 Internal Server Error` response.
+#[allow(clippy::needless_pass_by_value)]
+fn internal_error<E>(err: E) -> StatusCode
+where
+    E: Into<Error> + Display,
+{
+    error!(%err);
+    StatusCode::INTERNAL_SERVER_ERROR
 }
