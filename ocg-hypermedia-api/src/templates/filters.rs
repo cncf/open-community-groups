c2ca13b38e78c2a30dece000d2e761cd973614e4
Sergio Castaño Arteaga <tegioz@icloud.com>
2024-11-25 09:28:35 +0100

    Format stats numbers
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- ocg-hypermedia-api/src/templates/filters.rs
+++ ocg-hypermedia-api/src/templates/filters.rs
@@ -1,9 +1,16 @@
 //! Some custom filters for templates.
 
+use num_format::{Locale, ToFormattedString};
 use unicode_segmentation::UnicodeSegmentation;
 
 /// Return the string with all emojis removed.
 #[allow(clippy::unnecessary_wraps)]
 pub fn demoji(s: &str) -> askama::Result<String> {
     Ok(s.graphemes(true).filter(|gc| emojis::get(gc).is_none()).collect())
+}
+
+/// Format number according to international standards.
+#[allow(clippy::unnecessary_wraps)]
+pub fn num_fmt<T: ToFormattedString>(n: &T) -> askama::Result<String> {
+    Ok(n.to_formatted_string(&Locale::en))
 }
