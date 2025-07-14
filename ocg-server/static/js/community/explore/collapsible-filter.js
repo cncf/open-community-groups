f46c5e3b82bcd3305754fb22bd0c08bd5b96fb00
Cintia Sánchez García <cynthiasg@icloud.com>
2025-07-14 12:50:42 +0200

    Add collapsible-filter component
    Signed-off-by: Cintia Sánchez García <cynthiasg@icloud.com>

--- ocg-server/static/js/community/explore/collapsible-filter.js
+++ ocg-server/static/js/community/explore/collapsible-filter.js
@@ -12,7 +12,7 @@ export class CollapsibleFilter extends LitWrapper {
     maxVisibleItems: { type: Number },
     isCollapsed: { type: Boolean },
     viewType: { type: String },
-    multipleSelection: { type: Boolean },
+    singleSelection: { type: Boolean },
     visibleOptions: { type: Array },
     form: { type: String },
   };
@@ -28,7 +28,7 @@ export class CollapsibleFilter extends LitWrapper {
     this.isCollapsed = true;
     this.viewType = "cols";
     this.visibleOptions = [];
-    this.multipleSelection = true;
+    this.singleSelection = false;
     this.form = "";
   }
 
@@ -40,8 +40,23 @@ export class CollapsibleFilter extends LitWrapper {
   connectedCallback() {
     super.connectedCallback();
 
+    this._prepareSelected();
     this._checkMaxVisibleItems();
     this._filterOptions();
+  }
+
+  _prepareSelected() {
+    console.log("Preparing selected options:", this.selected);
+    if (this.selected === null || this.selected === undefined) {
+      this.selected = [];
+    } else if (typeof this.selected === "string" || typeof this.selected === "number") {
+      this.selected = [this.selected];
+    }
+    // if (typeof this.selected === "string") {
+    //   this.selected = [this.selected];
+    // } else {
+    //   this.selected = this.selected || [];
+    // }
   }
 
   _checkMaxVisibleItems() {
@@ -83,8 +98,8 @@ export class CollapsibleFilter extends LitWrapper {
     this._filterOptions();
   }
 
-  async _onSelect(value) {
-    if (this.multipleSelection) {
+  _onSelect(value) {
+    if (!this.singleSelection) {
       if (!this.selected.includes(value)) {
         this.selected = [...this.selected, value];
       } else {
@@ -95,14 +110,19 @@ export class CollapsibleFilter extends LitWrapper {
     }
     this._checkMaxVisibleItems();
     this._filterOptions();
+    this.requestUpdate();
+  }
 
-    // Wait for the update to complete
+  async _onSelectAny() {
+    // Clear all selections when "Any" is clicked
+    this.selected = [];
+    this._filterOptions();
+
+    // Request update and wait for it to complete
+    this.requestUpdate();
     await this.updateComplete;
 
-    console.log("Selected options:", this.selected);
-
-    // Trigger change event on the form
-    triggerChangeOnForm(this.form, "submit");
+    triggerChangeOnForm(this.form);
   }
 
   render() {
@@ -126,46 +146,44 @@ export class CollapsibleFilter extends LitWrapper {
                       class="svg-icon h-3 w-3 bg-gray-500 group-hover/btn:bg-white icon-caret-up"
                     ></div>`}
               </button>`
-            : None}
+            : ""}
         </div>
       </div>
       <ul class="flex w-full gap-2 mt-3 ${this.viewType === "rows" ? "flex-col" : "flex-wrap"}">
         <li>
-          <input
-            form="${this.form}"
-            type="checkbox"
-            name="any"
-            value=""
-            id="any-checkbox-${this.name}"
-            class="hidden peer"
-            ?checked=${this.selected.length === 0}
-          />
-          <label
-            for="any-checkbox-${this.name}"
-            class="inline-flex items-center justify-between w-full px-2 py-1 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer select-none peer-checked:border-primary-500 hover:text-gray-600 peer-checked:text-primary-500 hover:bg-gray-50"
+          <button
+            type="button"
+            @click=${this._onSelectAny}
+            class="inline-flex items-center justify-between w-full px-2 py-1 bg-white border border-gray-200 rounded-lg cursor-pointer select-none ${this
+              .selected.length === 0
+              ? "border-primary-500 text-primary-500"
+              : "text-gray-500 hover:text-gray-600 hover:bg-gray-50"}"
           >
             <div class="text-[0.8rem] text-center text-nowrap">Any</div>
-          </label>
+          </button>
         </li>
         ${repeat(
           this.visibleOptions,
           (opt) => opt,
           (opt) =>
             html`<li>
-              <button
-                type="button"
-                @click=${() => this._onSelect(opt.value)}
+              <label
                 class="inline-flex items-center justify-between w-full px-2 py-1 bg-white border border-gray-200 rounded-lg cursor-pointer select-none ${this.selected.includes(
                   opt.value,
                 )
                   ? "border-primary-500 text-primary-500"
                   : "text-gray-500 hover:text-gray-600 hover:bg-gray-50"}"
               >
+                <input
+                  type="checkbox"
+                  name="${this.name}"
+                  value="${opt.value}"
+                  .checked=${this.selected.includes(opt.value)}
+                  @change=${() => this._onSelect(opt.value)}
+                  class="sr-only"
+                />
                 <div class="text-[0.8rem] text-center text-nowrap capitalize">${opt.name}</div>
-              </button>
-              ${this.selected.includes(opt.value)
-                ? html`<input form="${this.form}" type="hidden" name="${this.name}" value="${opt.value}" />`
-                : ""}
+              </label>
             </li>`,
         )}
       </ul>
@@ -180,7 +198,7 @@ export class CollapsibleFilter extends LitWrapper {
               ${this.isCollapsed ? "+ Show more" : "- Show less"}
             </button>
           </div>`
-        : None}
+        : ""}
     </div>`;
   }
 }
