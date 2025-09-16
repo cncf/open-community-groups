18d7a3e8077c0b5f66e4e06bd16dcaa5b42dfe1b
Sergio Castaño Arteaga <tegioz@icloud.com>
2025-09-16 09:42:28 +0200

    Get always two initials
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- ocg-server/src/templates/dashboard/group/team.rs
+++ ocg-server/src/templates/dashboard/group/team.rs
@@ -6,11 +6,10 @@ use serde::{Deserialize, Serialize};
 use tracing::instrument;
 use uuid::Uuid;
 
-use crate::templates::{
-    filters,
-    helpers::{InitialsCount, user_initials},
+use crate::{
+    templates::{filters, helpers::user_initials},
+    types::group::{GroupRole, GroupRoleSummary},
 };
-use crate::types::group::{GroupRole, GroupRoleSummary};
 
 // Pages templates.
 
