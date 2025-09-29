6f8af26d1fd9d9a67f4e065fc94f5594529b007d
Cintia Sánchez García <cynthiasg@icloud.com>
2025-09-29 10:20:24 +0200

    Upgrade fullcalendar
    Signed-off-by: Cintia Sánchez García <cynthiasg@icloud.com>

--- ocg-server/static/js/community/explore/calendar.js
+++ ocg-server/static/js/community/explore/calendar.js
@@ -26,7 +26,7 @@ export class Calendar {
     // Load `fullcalendar` script
     let script = document.createElement("script");
     script.type = "text/javascript";
-    script.src = "/static/vendor/js/fullcalendar.v6.1.15.min.js";
+    script.src = "/static/vendor/js/fullcalendar.v6.1.19.min.js";
     document.getElementsByTagName("head")[0].appendChild(script);
 
     // Setup calendar after script is loaded
