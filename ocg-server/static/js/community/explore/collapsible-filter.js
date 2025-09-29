9d038a3e35fbcfb952726b92b84c5c264a035a41
Cintia Sánchez García <cynthiasg@icloud.com>
2025-09-29 12:18:08 +0200

    Upgrade lit
    Signed-off-by: Cintia Sánchez García <cynthiasg@icloud.com>

--- ocg-server/static/js/community/explore/collapsible-filter.js
+++ ocg-server/static/js/community/explore/collapsible-filter.js
@@ -1,4 +1,4 @@
-import { html, repeat } from "/static/vendor/js/lit-all.v3.2.1.min.js";
+import { html, repeat } from "/static/vendor/js/lit-all.v3.3.1.min.js";
 import { LitWrapper } from "/static/js/common/lit-wrapper.js";
 import { triggerChangeOnForm } from "/static/js/community/explore/filters.js";
 
