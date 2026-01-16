7ab59de24173085d9269ba7290f4667ed286de68
Cintia Sánchez García <cynthiasg@icloud.com>
2026-01-16 19:32:15 +0100

    Add test for new site page and fix some tests
    Signed-off-by: Cintia Sánchez García <cynthiasg@icloud.com>

--- tests/e2e/public/public.spec.ts
+++ tests/e2e/public/public.spec.ts
@@ -1,58 +1,71 @@
 import { expect, test } from "@playwright/test";
 
 import {
+  TEST_COMMUNITY_NAME,
   TEST_COMMUNITY_TITLE,
   TEST_EVENT_NAME,
   TEST_EVENT_SLUG,
   TEST_GROUP_NAME,
   TEST_GROUP_SLUG,
   TEST_SEARCH_QUERY,
-  TEST_COMMUNITY_HOST,
+  TEST_SITE_TITLE,
+  navigateToCommunityHome,
   navigateToEvent,
-  navigateToExplore,
   navigateToGroup,
-  navigateToHome,
   navigateToPath,
-  setHostHeader,
+  navigateToSiteExplore,
+  navigateToSiteHome,
 } from "../utils";
 
 test.describe("public pages", () => {
-  test.beforeEach(async ({ page }) => {
-    await setHostHeader(page, TEST_COMMUNITY_HOST);
-  });
-
-  test("home page loads and displays correctly", async ({ page }) => {
-    await navigateToHome(page);
+  test("site home page loads and displays correctly", async ({ page }) => {
+    await navigateToSiteHome(page);
 
     await expect(
-      page.getByRole("heading", { level: 1, name: TEST_COMMUNITY_TITLE })
+      page.getByRole("heading", { level: 1, name: TEST_SITE_TITLE })
     ).toBeVisible();
-    await expect(page.getByRole("link", { name: "Explore all" })).toBeVisible();
+    await expect(
+      page.getByRole("link", { name: TEST_COMMUNITY_TITLE })
+    ).toBeVisible();
+  });
+
+  test("community home page loads and displays correctly", async ({ page }) => {
+    await navigateToCommunityHome(page, TEST_COMMUNITY_NAME);
+
+    await expect(page.getByText(TEST_COMMUNITY_TITLE)).toBeVisible();
+    await expect(
+      page.getByRole("link", { name: "Explore all groups" })
+    ).toBeVisible();
   });
 
   test("explore page loads for events and groups", async ({ page }) => {
-    await navigateToExplore(page);
+    await navigateToSiteExplore(page);
     await expect(page.getByPlaceholder("Search events")).toBeVisible();
     await expect(
-      page.getByRole("heading", { level: 1, name: TEST_COMMUNITY_TITLE })
+      page.getByRole("heading", { level: 1, name: TEST_SITE_TITLE })
     ).toBeVisible();
 
     await navigateToPath(page, "/explore?entity=groups");
     await expect(page.getByPlaceholder("Search groups")).toBeVisible();
     await expect(
-      page.getByRole("heading", { level: 1, name: TEST_COMMUNITY_TITLE })
+      page.getByRole("heading", { level: 1, name: TEST_SITE_TITLE })
     ).toBeVisible();
   });
 
   test("group page displays group information", async ({ page }) => {
-    await navigateToGroup(page, TEST_GROUP_SLUG);
+    await navigateToGroup(page, TEST_COMMUNITY_NAME, TEST_GROUP_SLUG);
     await expect(
       page.getByRole("heading", { level: 1, name: TEST_GROUP_NAME })
     ).toBeVisible();
   });
 
   test("event page displays event information", async ({ page }) => {
-    await navigateToEvent(page, TEST_GROUP_SLUG, TEST_EVENT_SLUG);
+    await navigateToEvent(
+      page,
+      TEST_COMMUNITY_NAME,
+      TEST_GROUP_SLUG,
+      TEST_EVENT_SLUG
+    );
     await expect(
       page.getByRole("heading", { level: 1, name: TEST_EVENT_NAME })
     ).toBeVisible();
@@ -63,6 +76,6 @@ test.describe("public pages", () => {
       page,
       `/explore?entity=groups&ts_query=${encodeURIComponent(TEST_SEARCH_QUERY)}`
     );
-    await expect(page.getByRole("link", { name: TEST_GROUP_NAME })).toBeVisible();
+    await expect(page.getByText(TEST_GROUP_NAME)).toBeVisible();
   });
 });
