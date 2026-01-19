6f4e07254a7efeb33742fb0460d0e43173581540
Cintia Sánchez García <cynthiasg@icloud.com>
2026-01-19 07:44:49 +0100

    Fix some tests
    Signed-off-by: Cintia Sánchez García <cynthiasg@icloud.com>

--- tests/e2e/dashboard/access-control.spec.ts
+++ tests/e2e/dashboard/access-control.spec.ts
@@ -1,18 +1,14 @@
 import { expect, test } from "@playwright/test";
 
-import {
-  TEST_COMMUNITY_HOST,
-  navigateToPath,
-  setHostHeader,
-} from "../utils";
+import { navigateToPath } from "../utils";
 
-const dashboardRoutes = ["/dashboard/community", "/dashboard/group", "/dashboard/user"];
+const dashboardRoutes = [
+  "/dashboard/community",
+  "/dashboard/group",
+  "/dashboard/user",
+];
 
 test.describe("dashboard access control", () => {
-  test.beforeEach(async ({ page }) => {
-    await setHostHeader(page, TEST_COMMUNITY_HOST);
-  });
-
   for (const route of dashboardRoutes) {
     test(`requires login for ${route}`, async ({ page }) => {
       await navigateToPath(page, route);
